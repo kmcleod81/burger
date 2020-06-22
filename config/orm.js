768aab83becc22a98ab2f3e3d71237fbcf7bc3c1
kdawg <mcleod.karla@gmail.com>
2020-06-22 14:06:25 -0400

    Completed config setup - orm.js

--- config/orm.js
+++ config/orm.js
@@ -1,9 +1,31 @@
 const connection = require("./connection");
 
-// selectAll()
-
-// insertOne()
-
-// updateOne()
+const orm = {
+    // selectAll()
+    selectAll: (tableInput, cb) => {
+        const queryString = "SELECT * FROM " + tableInput + ";";
+        connection.query(queryString, (err, result) => {
+            if (err) throw err;
+            cb(result);
+        });
+    },
+    // insertOne()
+    insertOne: (table, cols, vals, cb) => {
+        const queryString = "INSERT INTO ?? (??) VALUES (?)";
+        connection.query(queryString, [table, cols, vals], (err, result) => {
+            if (err) throw err;
+            cb(result);
+        });
+    },
+    // updateOne()
+    updateOne: (table, updateColVal, updateValue, id, cb) => {
+        const queryString = "UPDATE ?? SET ?? = ? WHERE id = ?";
+        connection.query(queryString, [table, updateColVal, updateValue, id], (err, result) => {
+            if (err) throw err;
+            cb(result);
+        });
+    }
+};
 
 module.exports = orm;
+
